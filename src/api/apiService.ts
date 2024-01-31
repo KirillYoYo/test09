61ce5f20d50291ff75054a0fde5680679c87c9f4
kmyasnikov <K.Myasnikov@ase-ec.ru>
2024-01-31 22:28:29 +0300

    add signal to API

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -6,9 +6,12 @@ export interface CountryInfo {
   flag: string;
 }
 
-export function getCountryByName(countryName: string): Promise<CountryInfo[]> {
-  return new Promise((resolve) => {
+export function getCountryByName(countryName: string, signal: any): Promise<CountryInfo[]> {
+  return new Promise((resolve, reject) => {
     setTimeout(resolve, getRandom(100, 800));
+    signal.addEventListener('abort', () => {
+      reject("Promise aborted")
+    });
   }).then(() => {
     if (typeof countryName !== "string" || !countryName) {
       return [];
